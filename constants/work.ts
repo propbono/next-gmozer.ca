e12087c8e6cfedfc3b2a4baee0afece96c2030b8
propbono <propbono@gmail.com>
2024-10-01 15:35:16 -0400

    feat: add image prop

---
+++ constants/work.ts
@@ -0,0 +1,49 @@
+export const PROJECTS = [
+  {
+    title: "Farah Freight Group",
+    category: "Frontend",
+    description: "Company website for Farah Freight Group. Single Page Application created using NextJS and Nodemailer for sending emails.",
+    liveLink: "https://farahfg.com/",
+    githubLink: "https://github.com/propbono/farahfg.com",
+    stack: ["React", "Next.js", "TypeScript", "Tailwind CSS", "Nodemailer", "Vercel"],
+    image: "/work/p1.jpg",
+  },
+  {
+    title: "Gemprint",
+    category: "Fullstack",
+    description:
+      "Company website for Gemprint. Single Page Application created using NextJS and NextAuth. Currently working on adding Dashboard to manage print orders.",
+    liveLink: "https://gemprint.ca/",
+    githubLink: "https://github.com/propbono/gemprint.ca",
+    stack: ["Next.js", "TypeScript", "Tailwind CSS", "Prisma", "Vercel"],
+    image: "/work/p2.jpg",
+  },
+  {
+    title: "Wizzcode Music App",
+    category: "Frontend",
+    description:
+      "Sample music app to test React Query hooks and write some cool tests in Vitest. It communicates with Itunes API and pull some albums using React Query.",
+    liveLink: "https://wizcode-music.vercel.app/",
+    githubLink: "https://github.com/propbono/gmozer.ca",
+    stack: ["Remix.js", "TypeScript", "Tailwind CSS", "Vite", "Vercel"],
+    image: "/work/p3.jpg",
+  },
+  {
+    title: "Propbono Portfolio - new",
+    category: "Fullstack",
+    description: "Propbono portfolio website presenting some of the projects that I worked on.",
+    liveLink: "https://gmozer.ca/",
+    githubLink: "https://github.com/propbono/next-gmozer.ca",
+    stack: ["Next.js", "TypeScript", "Tailwind CSS", "FramerMotion", "Vercel"],
+    image: "/work/p4.jpg",
+  },
+  {
+    title: "Propbono Portfolio - old",
+    category: "Frontend",
+    description: "Propbono portfolio website presenting some of the projects that I worked on. Written in RemixJS.",
+    liveLink: "https://gmozer-old.vercel.app/",
+    githubLink: "https://github.com/propbono/gmozer.ca",
+    stack: ["Remix.js", "TypeScript", "Tailwind CSS", "Vite", "Vercel"],
+    image: "/work/p5.jpg",
+  },
+];
