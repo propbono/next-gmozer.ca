6c78ae38811ba1898df7b3d49ac4fb1f636e727b
propbono <propbono@gmail.com>
2024-10-18 20:12:31 -0400

    fix: update layout shift

--- components/stats/index.tsx
+++ components/stats/index.tsx
@@ -4,11 +4,11 @@ import { type Stat } from "@/types/stats";
 import CountUp from "react-countup";
 export const Stats = ({ stats }: { stats: Stat[] }) => {
   return (
-    <div className="flex flex-col md:flex-row md:flex-wrap md:justify-between mx-auto max-w-64 md:max-w-none justify-self-center gap-4">
+    <div className="flex flex-col md:flex-row md:flex-wrap w-full gap-4 justify-between">
       {stats.map((stat) => (
         <div
           key={stat.title}
-          className="flex flex-row gap-4 items-center"
+          className="flex flex-row gap-4 items-center justify-end"
         >
           <CountUp
             end={stat.value}
@@ -17,7 +17,7 @@ export const Stats = ({ stats }: { stats: Stat[] }) => {
             separator=" "
             className="text-4xl md:text-6xl font-extrabold text-primary"
           />
-          <span className={cn("text-lg md:max-w-16 font-medium text-foreground/90")}>
+          <span className={cn("text-lg md:max-w-32 font-medium text-foreground/90")}>
             {stat.title}
           </span>
         </div>
