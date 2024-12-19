a2343a5f53c0275ad18ef3683c4fb919bc434116
propbono <propbono@gmail.com>
2024-12-19 22:53:36 +0100

    feature(stats): remove layout shift

--- components/stats/index.tsx
+++ components/stats/index.tsx
@@ -4,19 +4,21 @@ import { type Stat } from "@/types/stats";
 import CountUp from "react-countup";
 export const Stats = ({ stats }: { stats: Stat[] }) => {
   return (
-    <div className="flex flex-col md:flex-row md:flex-wrap w-full gap-4 justify-between">
+    <div className="grid grid-cols-2 md:flex md:flex-row md:flex-wrap w-full gap-4 justify-between">
       {stats.map((stat) => (
         <div
           key={stat.title}
-          className="flex flex-row gap-4 items-center justify-end"
+          className="flex flex-row gap-4 items-center justify-start md:justify-end"
         >
-          <CountUp
-            end={stat.value}
-            duration={5}
-            delay={1}
-            separator=" "
-            className="text-4xl md:text-6xl font-extrabold text-primary"
-          />
+          <div className="min-w-[4ch] font-mono text-right">
+            <CountUp
+              end={stat.value}
+              duration={5}
+              delay={1}
+              separator=" "
+              className="text-4xl md:text-6xl font-extrabold text-primary"
+            />
+          </div>
           <span className={cn("text-lg md:max-w-32 font-medium text-foreground/90")}>
             {stat.title}
           </span>
