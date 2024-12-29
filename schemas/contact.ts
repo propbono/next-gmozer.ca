d46e579d4e06a8bd73ee6ec59e45eff074720a4f
propbono <propbono@gmail.com>
2024-12-29 22:31:28 +0100

    fix: update error message

--- schemas/contact.ts
+++ schemas/contact.ts
@@ -4,7 +4,7 @@ export const contactFormSchema = z.object({
   fullname: z.string().min(2, "Name must be at least 2 characters"),
   email: z.string().email("Invalid email address"),
   message: z.string().min(10, "Message must be at least 10 characters"),
-  website: z.string().max(0, "Something went wrong."),
+  website: z.string().max(0, "Something went wrong"),
 });
 
 export type ContactFormData = z.infer<typeof contactFormSchema>;
